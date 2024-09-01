d8ca21e58852834558216db2ac3c3180179cfbeb
Cletus-05 <cletusfernandes0520@gmail.com>
2024-09-01 09:55:44 +0530

    updated

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Navbar from '../Components/Navbar/Navbar'
 
 const Shop = () => {
   return (
     <div>
-      
+      <Hero/>
     </div>
   )
 }
