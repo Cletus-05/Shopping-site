f783886516a224c70cc90e8a759a44a2a0179861
Cletus-05 <cletusfernandes0520@gmail.com>
2024-09-15 22:38:46 +0530

    updated

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">
+          Description
+        </div>
+        <div className="descriptionbox-nav-box fade">
+          Reviews (122)
+        </div>
+      </div>
+      <div className='descriptionbox-description'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro rem cumque nemo beatae doloremque voluptas nostrum modi autem. Animi, perferendis repudiandae. Necessitatibus, officiis! Quis deleniti illum maiores repellat consequatur tenetur!
+        Tempore error quas, ipsum odio quibusdam aut magni porro repellendus nobis enim, itaque reprehenderit quam in voluptatem, placeat eos nisi dolor doloremque autem voluptates! Itaque nesciunt rerum perferendis perspiciatis praesentium.
+        Quis unde, debitis architecto suscipit cumque minus magnam. Aliquam corporis, quos quisquam quae dicta autem neque. Non placeat quisquam amet, dolore nemo, accusamus quasi necessitatibus quia voluptatum porro voluptatibus est!
+        Tempore dolores minima mollitia, ipsa nemo voluptatem nesciunt fugit magnam fugiat qui blanditiis incidunt ullam odit. Maxime reiciendis blanditiis consectetur ex fuga magnam quaerat non, nobis nihil sapiente laboriosam ratione?
+        Totam, illum explicabo blanditiis quisquam asperiores neque non, nemo voluptatem nulla sit omnis voluptatibus! Sapiente, voluptate sed, obcaecati error blanditiis minima doloribus sequi perferendis quae, minus distinctio veritatis fuga quis.</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
