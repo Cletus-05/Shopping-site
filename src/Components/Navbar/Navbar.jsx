12e4151c8a6d7a01ed48b666b84917f0ed7f8cab
Cletus-05 <cletusfernandes0520@gmail.com>
2024-09-18 19:55:00 +0530

    updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/nav_dropdown.png'
 
 
 const Navbar = () => {
 
     const [menu,setMenu]=useState("shop");
+    const {getTotalCartItems}= useContext(ShopContext);
+    const menuRef=useRef();
+
+    const dropdown_toggle=(e)=>{
+      menuRef.current.classList.toggle('nav-menu-visible');
+      e.target.classList.toggle('open');
+
+    }
 
   return (
     <div className='navbar' >
@@ -16,7 +26,8 @@ const Navbar = () => {
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={()=>{setMenu("shop")}}><Link to='/' style={{textDecoration:'none'}}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("mens")}}><Link to='/mens' style={{textDecoration:'none'}}>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("womens")}}><Link to='/womens' style={{textDecoration:'none'}}>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
@@ -25,7 +36,7 @@ const Navbar = () => {
       <div className="nav-login-cart">
         <Link to='/login'><button>Login</button></Link>
         <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   )
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -34,10 +34,20 @@ const ShopContextProvider = (props) =>{
         return totalAmount;
     }
 
-    const contextValue={getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
+    const getTotalCartItems=()=>{
+        let totalItem=0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                totalItem+=cartItems[item];;
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue={getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
     return(
         <ShopContext.Provider value={contextValue}>
-            {props.children}  //why have we used props here?
+            {props.children}  
         </ShopContext.Provider>
     )
 }
